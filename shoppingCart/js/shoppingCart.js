c32d17ecd9e658d46814d31799f59d3c618210c1
Sujeong Cho <112532259+sujeongcho@users.noreply.github.com>
2022-12-09 13:02:41 +0900

    Update shoppingCart.js

--- shoppingCart/js/shoppingCart.js
+++ shoppingCart/js/shoppingCart.js
@@ -1,57 +1,18 @@
- const plus = document.querySelectorAll(".plus");
- const minus = document.querySelectorAll(".minus");
- const num = document.querySelectorAll(".num");
- const p_price = document.querySelectorAll('.p_price');
- const t_price = document.querySelectorAll(".t_price");
- const c_price = document.querySelector('.Cprice');
- const TT_price = document.querySelector('.TTprice');
+var count = 1;
+var countEl = document.getElementById("count");
+var total_count = document.getElementById("total_count");
+var total_count_view = document.getElementById("total_count_view");
 
-// let a =1;
-
-let sum = 0;
-console.log(p_price);
-p_price.forEach(ele =>{
-    sum += parseInt(ele.innerText);
-})
-c_price.innerText = sum;
-
-console.log(plus);
-
-for (let i = 0; i < plus.length; i++) {
-    plus[i].addEventListener("click", () => {
-        let a = parseInt(num[i].innerText);
-        a++;
-        a = (a < 10)
-            ? "0" + a
-            : a;
-        num[i].innerText = a;
-        console.log(a);
-        const priceP = parseInt(p_price[i].innerText);
-        const priceT = priceP*parseInt(num[i].innerText);
-        const priceC = parseInt( c_price.innerText);
-        
-         t_price[i].innerText = priceT;
-         c_price.innerText = priceC+priceP;
-    });
+function plus(){
+    count++;
+    countEl.value = count;
+    total_count_view.value = (total_count.value * countEl.value).toLocaleString();
+}
+function minus(){
+    if(count > 1){
+        count--;
+        countEl.value = count;
+        total_count_view.value = (total_count.value * countEl.value).toLocaleString();
+    }
 }
 
-  
- for (let i = 0; i < minus.length; i++) {
-    minus[i].addEventListener("click", () => {
-        let a = parseInt(num[i].innerText);
-        if (a > 1) {
-            a--;
-            a = (a < 10)
-                ? "0" + a
-                : a;
-            num[i].innerText = a;
-        }
-
-        const priceP = parseInt(p_price[i].innerText);
-        const priceT = priceP*parseInt(num[i].innerText);
-        const priceC = parseInt( c_price.innerText);
-        
-         t_price[i].innerText = priceT;
-         c_price.innerText = priceC-priceP;
-    })
-}
